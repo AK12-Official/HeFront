799e15c3b092926f8a1b653014479dfb79564b67
AK12_Official <995929122@qq.com>
2025-06-26 10:55:20 +0800

    fix for Actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
+  // @ts-expect-error: 类型定义不完全匹配，但实际上可以正常工作
   viteConfig,
   defineConfig({
     test: {
@@ -10,5 +11,5 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
-  }),
+  })
 )

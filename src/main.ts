def4cbf67de53b7eab2344956745002f49907c3c
AK12_Official <995929122@qq.com>
2025-06-28 01:06:35 +0800

    新增统一状态管理仓库

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import ElementPlus from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn' //配置国际化 引入中文语言包
 import router from '@/router' //引入路由器对象
 import 'element-plus/dist/index.css';
+import pinia from "./store";
 
 const app = createApp(App);
-
+app.use(pinia)
 //安装ElementPlus插件
 app.use(ElementPlus, {
   locale: zhCn,
